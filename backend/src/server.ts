350a85cbe213175ceff4824bb0916e6c9be3aa08
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-06-03 18:12:55 -0300

    Adicion arquivos SVG e rota que exibe o arquivo para o ser renderizado automaticamente, sem a necessidade do download do arquivo.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server Running at Port 3333')
+});
