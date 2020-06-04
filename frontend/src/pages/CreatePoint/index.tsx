e5e2f4c238d56ef5b6e106dd6fb5c4ed68b757e7
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-06-04 16:09:19 -0300

    Integra o mapa dentro do formulário. Map este do leaflet open source, gratuito e pra javascript

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css'
 
@@ -57,6 +58,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o Endereço no mapa</span>
           </legend>
+
+          <Map center={[-23.6641927, -46.7768741]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-23.6641927, -46.7768741]}/>
+          </Map>
 
           <div className="field-group">
             <div className="field">
